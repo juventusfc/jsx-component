7db6a86a4d7eb3bf4b1fed9c8e2bb06a6aa37163
FrankHu <frank.hu@perficient.com>
2020-08-20 15:20:00 +0800

    re

--- main.js
+++ main.js
@@ -2,59 +2,6 @@ import { createElement } from "./tools/createElement";
 import Div from "./components/Div";
 import MyComponent from "./components/MyComponent";
 
-/** 
-// 1. 解决大写的组件
-let component = (
-  <Div
-    name="frank"
-    style="width:200px; height: 100px; background-color: red;"
-  ></Div>
-);
-
-// 2. 解决小写的组件
-let component = (
-  <div
-    name="frank"
-    style="width:200px; height: 100px; background-color: red;"
-  ></div>
-);
-*/
-
-// 3. 解决 children 以及嵌套
-// let component = (
-//   <Div
-//     id="large1"
-//     style="width:200px; height: 200px; background-color: purple;"
-//   >
-//     <Div
-//       id="middle1"
-//       style="width:20px; height: 20px; background-color: blue;"
-//     ></Div>
-//     <Div
-//       id="middle2"
-//       style="width:20px; height: 20px; background-color: yellow;"
-//     >
-//       <Div
-//         id="small1"
-//         style="width:10px; height: 10px; background-color: red;"
-//       ></Div>
-//     </Div>
-//     <Div
-//       id="middle3"
-//       style="width:20px; height: 20px; background-color: green;"
-//     ></Div>
-//   </Div>
-// );
-
-// 4. 解决 text
-// let component = (
-//   <Div name="frank" style="width:200px; height: 100px; background-color: red;">
-//     hello world<span>hello again</span>
-//     <div>forza juventus</div>
-//   </Div>
-// );
-
-// 5. 解决 Mycomponent 中还有其他自定义的 header 和 footer
 let component = (
   <div id="outer">
     <MyComponent>
