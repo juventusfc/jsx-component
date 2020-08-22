c3364717518fbffb5999b36d2dcdd6605c0b5ba2
FrankHu <frank.hu@perficient.com>
2020-08-22 21:15:43 +0800

    ref

--- components/Div.js
+++
@@ -1,7 +0,0 @@
-import Component from "../utils/Component";
-export default class Div extends Component {
-  constructor() {
-    super();
-    this.rootDom = document.createElement("div");
-  }
-}
--- main.js
+++ main.js
@@ -1,35 +1,5 @@
 import { createElement } from "./utils/createElement";
-// import Div from "./components/Div";
-// import MyComponent from "./components/MyComponent";
 import VanillaCarousel from "./components/VanillaCarousel";
-
-// let component = (
-//   <div id="outer">
-//     <VanillaCarousel
-//       data={[
-//         "https://static001.geekbang.org/resource/image/bb/21/bb38fb7c1073eaee1755f81131f11d21.jpg",
-//         "https://static001.geekbang.org/resource/image/1b/21/1b809d9a2bdf3ecc481322d7c9223c21.jpg",
-//         "https://static001.geekbang.org/resource/image/b6/4f/b6d65b2f12646a9fd6b8cb2b020d754f.jpg",
-//         "https://static001.geekbang.org/resource/image/73/e4/730ea9c393def7975deceb48b3eb6fe4.jpg",
-//       ]}
-//     />
-//     <MyComponent>
-//       <div>text text text text</div>
-//     </MyComponent>
-//     <div
-//       id="a"
-//       class="b"
-//       style="width:200px; height: 100px; border: solid 1px red;"
-//     >
-//       <Div>
-//         <div>text1</div>
-//       </Div>
-//       <p></p>
-//       <div></div>
-//       <Div></Div>
-//     </div>
-//   </div>
-// );
 
 let component = (
   <VanillaCarousel
